8dc973a6ae187b090c83f47c398430522f31cd5d
Cedric Porter <cedricporter@gmail.com>
2014-08-28 15:46:41 +0800

    monitor 60000-60029

--- port-ip-monitor.sh
+++ port-ip-monitor.sh
@@ -6,7 +6,7 @@
 #
 
 filename="port-ip-monitor.log"
-regex="(909|600[0-9]{2})"
+regex="600[0-2][0-9]"  # monitor 60000-60029
 
 date +"[%Y-%m-%d %H:%M:%S]" >> $filename
 netstat -anp | egrep $regex | grep -E "tcp.*ESTABLISHED" | awk '{print $4, $5}' | cut -d: -f2 | sort -u >> $filename
